14f8979b83f76afcce24e123e87aac99febb3a7f
vaibhav <vaibhavsingh8931@gmail.com>
2026-01-27 06:49:43 +0530

    Restructure repo into monorepo with downstream service

---
+++ DownStream-Service/src/main/java/com/downstream/DownStream_Service/DownStreamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.downstream.DownStream_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownStreamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownStreamServiceApplication.class, args);
+	}
+
+}
---
+++ DownStream-Service/src/test/java/com/downstream/DownStream_Service/DownStreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downstream.DownStream_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownStreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
